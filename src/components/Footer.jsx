e9d3330391e00488c63b80b5bbb7817d98b48199
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-05-21 22:29:28 +0530

    kiiiii

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import "../index.css";
 
 const Footer = () => {
   return (
-    <div className="reply">
+    <footer className="reply">
       <input className="send" type="text" placeholder="Send your Love..." />
-    </div>
+    </footer>
   );
 };
 
