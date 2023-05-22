9224c6bc160f5383ff259c2bc8610813d5da32cd
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-05-22 18:23:57 +0530

    gsg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "../index.css";
+import AttachFileRoundedIcon from "@mui/icons-material/AttachFileRounded";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
 
 const Footer = () => {
   return (
     <footer className="reply">
       <input className="send" type="text" placeholder="Send your Love..." />
+      <div className="paper-clip">
+        <AttachFileRoundedIcon
+          style={{
+            transform: "rotate(45deg)",
+            backgroundColor: "#ffffff",
+            background: "none",
+          }}
+        />
+      </div>
+      <div className="send-arrow">
+        <SendRoundedIcon
+          style={{ backgroundColor: "#ffffff", border: "none" }}
+        />
+      </div>
     </footer>
   );
 };
