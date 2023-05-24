309b70833795b53931803bcc34c6e6b09350f5da
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-05-24 10:03:20 +0530

    Almost at the end

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "../index.css";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useRef } from "react";
+// import { useRef } from "react";
 import axios from "axios";
 
 const Chat = () => {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [data, setData] = useState([]);
-  const containerRef = useRef(null);
+  // const containerRef = useRef(null);
 
   useEffect(() => {
     const fetchMessages = async (page) => {
@@ -25,33 +25,49 @@ const Chat = () => {
     fetchMessages(page);
   }, [page]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const container = containerRef.current;
-      if (
-        container.scrollTop + container.clientHeight >=
-        container.scrollHeight - 200
-      ) {
-        setPage((prevPage) => prevPage + 1);
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const container = containerRef.current;
+  //     if (
+  //       container.scrollTop + container.clientHeight >=
+  //       container.scrollHeight - 200
+  //     ) {
+  //       setPage((prevPage) => prevPage + 1);
+  //     }
+  //   };
 
-    const container = containerRef.current;
-    container.addEventListener("scroll", handleScroll);
-    return () => {
-      container.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  //   const container = containerRef.current;
+  //   container.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     container.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  window.addEventListener("scroll", () => {
+    // Calculate the scroll position and height of the page
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight) {
+      setPage((page) => page + 1);
+    }
+  });
+
+  // data.sort(function (a, b) {
+  //   return a.time < b.time;
+  // });
 
   const renderChats = () => {
     return data.map((chats) =>
       chats.sender.self === true ? (
-        <div className="self">
+        <div key={chats.id} className="self">
           <div className="context-self">{chats.message}</div>
+          <p>{chats.time}</p>
         </div>
       ) : (
-        <div className="other">
+        <div key={chats.id} className="other">
           <div className="context">{chats.message}</div>
+          <p>{chats.time}</p>
           <div className="icon">
             <img src={chats.sender.image} alt="" className="pic" />
           </div>
@@ -60,11 +76,7 @@ const Chat = () => {
     );
   };
 
-  return (
-    <div ref={containerRef} className="chat-body">
-      {renderChats()}
-    </div>
-  );
+  return <div className="chat-body">{renderChats()}</div>;
 };
 
 export default Chat;
